be1bdaf9c97cc173f29461e6cddda96f5fbce817
ErickProg23 <prog.erick.mtz@gmail.com>
2026-02-17 11:13:40 -0800

    Cambios para la BD

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:admin123@localhost/eva-estaciones'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:admin123@localhost/pruebas_eva_estaciones'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
